3a2100334c57d6f68627aff8a8464e9f37488018
GustavoeDev <gustavoe.dev@gmail.com>
2025-06-21 12:25:39 -0300

    feat: configure RabbitMQ integration and implement order processing simulation

---
+++ order-processings-service/src/test/java/br/com/gustavoedev/order_processings_service/OrderProcessingsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavoedev.order_processings_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderProcessingsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
